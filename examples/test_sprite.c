1634bc8c67ff2380b8e21f2ee2143994c5516d11
Bruno STEUX <B.STEUX@nexter-group.fr>
2023-04-03 16:50:19 +0200

    added PAL version

--- examples/test_sprite.c
+++ examples/test_sprite.c
@@ -3,27 +3,36 @@
 char i;
 unsigned char X, Y;
 unsigned char xpos, ypos;
-char dlend[12];
 char *dlpnt;
 
 holeydma scattered(16, 1) char sprite[16] = { 0x3c, 0x3c, 0x42, 0x42, 0x99, 0x99, 0xa5, 0xa5, 0x81, 0x81, 0xa5, 0xa5, 0x42, 0x42, 0x3c, 0x3c };
-ramchip char dll[16 * 3];
 ramchip char dl0[64];
 ramchip char dl1[64], dl2[64], dl3[64], dl4[64], dl5[64], dl6[64], dl7[64], dl8[64], dl9[64], dl10[64], dl11[64];
-const char *dls[12] = { dl0, dl1, dl2, dl3, dl4, dl5, dl6, dl7, dl8, dl9, dl10, dl11 };
+#ifdef PAL
+#define DLL_ARRAY_SIZE 15 
+#define YMAX (240 - 16)
+ramchip char dl12[64], dl13[64], dl14[64];
+const char *dls[DLL_ARRAY_SIZE] = { dl0, dl1, dl2, dl3, dl4, dl5, dl6, dl7, dl8, dl9, dl10, dl11, dl12, dl13, dl14 };
+#else
+#define DLL_ARRAY_SIZE 12 
+#define YMAX (192 - 16)
+const char *dls[DLL_ARRAY_SIZE] = { dl0, dl1, dl2, dl3, dl4, dl5, dl6, dl7, dl8, dl9, dl10, dl11 };
+#endif
+ramchip char dll[(DLL_ARRAY_SIZE + 4) * 3];
+char dlend[DLL_ARRAY_SIZE];
 
 void main()
 {
     // Build DLL
-    // 20 blank lines
+    // 25 blank lines
     dll[X = 0] = 0x4f;  // 16 lines
     dll[++X] = 0x21; // 0x2100 = Blank DL
     dll[++X] = 0x00;
-    dll[++X] = 0x44; // 4 lines
+    dll[++X] = 0x49; // 4 lines
     dll[++X] = 0x21; // 0x2100 = Blank DL
     dll[++X] = 0x00;
     // 192 lines divided into 12 regions
-    for (Y = 0; Y != 12; Y++) {
+    for (Y = 0; Y != DLL_ARRAY_SIZE; Y++) {
         dll[++X] = 0x4f; // 16 lines
         dll[++X] = dls[Y] >> 8; // High address
         dll[++X] = dls[Y]; // Low address
@@ -61,14 +70,14 @@ void main()
             xpos--;
         }
         if (!(*SWCHA & 0x20)) { // Pushed down ?
-            if (ypos != 176) ypos++;
+            if (ypos != YMAX) ypos++;
         }
         if (!(*SWCHA & 0x10)) { // Pushed up ?
             if (ypos != 0) ypos--;
         }
 
         // Reset DL ends
-        for (X = 11; X >= 0; X--) {
+        for (X = DLL_ARRAY_SIZE - 1; X >= 0; X--) {
             dlend[X] = 0;
         }
 
@@ -99,7 +108,7 @@ void main()
         }
 
         // Add DL end entry on each DL
-        for (X = 11; X >= 0; X--) {
+        for (X = DLL_ARRAY_SIZE - 1; X >= 0; X--) {
             dlpnt = dls[X];
             Y = dlend[X];
             dlpnt[++Y] = 0; 
