877500c05300d53b0a2928f67b50e7dcb7bcd5bd
Bruno STEUX <bruno.steux@gmail.com>
2023-06-12 16:33:03 +0200

    new tiling code. Much better

--- headers/tiling.h
+++ headers/tiling.h
@@ -61,25 +61,62 @@ void _tiling_goto()
     for (_tiling_tmp = 0; _tiling_tmp < _tiling_ypos; _tiling_tmp++) {
        _tiling_ptr += _tiling_width;
     }
-    _tiling_ptr += _tiling_xpos;
     X = _MS_TOP_SCROLLING_ZONE; 
     if (_ms_buffer) X += _MS_DLL_ARRAY_SIZE; 
     _ms_tmpptr = _ms_dls[X]; 
-    _ms_tmp2 = _ms_tmpptr[Y = 3]; // Get the palette and width
+#define palette_and_width _ms_tmp2
+    palette_and_width = _ms_tmpptr[Y = 3]; // Get the palette and width
+    if (_tiling_ypos < 0) {
+        // Fill with blank
+        _ms_tmp3 = _MS_TOP_SCROLLING_ZONE - _tiling_ypos;
+        for (_ms_tmp = _MS_TOP_SCROLLING_ZONE; _ms_tmp < _ms_tmp3; _ms_tmp++) {
+            _ms_tmpptr = _ms_dls[X++]; 
+            Y = 3; 
+            _ms_tmpptr[Y++] = palette_and_width | 0x1f; 
+            _ms_tmpptr[Y] = 160; // Out of screen 
+        }
+    } else {
+        _ms_tmp = _MS_TOP_SCROLLING_ZONE;
+    }
+    if (_tiling_xpos + 21 >= _tiling_width) {
+        palette_and_width = palette_and_width & 0xe0 | ((_tiling_xpos - _tiling_width) & 0x1f);
+    } else {
+        palette_and_width = palette_and_width & 0xe0 | (-21 & 0x1f);
+    }
     _tiling_tmp = -_tiling_xoffset;
-    for (_ms_tmpptr2 = _tiling_ptr, _ms_tmp = _MS_TOP_SCROLLING_ZONE; _ms_tmp != _MS_DLL_ARRAY_SIZE; _ms_tmp++) {
+    if (_tiling_xpos > 0) {
+        _tiling_ptr += _tiling_xpos;
+    } else {
+        _tiling_tmp -= (_tiling_xpos << 3);
+    }
+#define bottom _ms_tmp3
+    if (_tiling_ypos + _MS_DLL_ARRAY_SIZE - _MS_TOP_SCROLLING_ZONE >= _tiling_height) {
+        bottom = _MS_TOP_SCROLLING_ZONE + _tiling_height - _tiling_ypos;
+    } else {
+        bottom = _MS_DLL_ARRAY_SIZE;
+    }
+    for (_ms_tmpptr2 = _tiling_ptr; _ms_tmp < bottom; _ms_tmp++) {
         _ms_tmpptr = _ms_dls[X++]; 
         Y = 0; 
         _ms_tmpptr[Y++] = _ms_tmpptr2; 
         Y++;
         _ms_tmpptr[Y++] = _ms_tmpptr2 >> 8; 
-        _ms_tmpptr[Y++] = _ms_tmp2; 
-        _ms_tmpptr[Y++] = _tiling_tmp; 
+        _ms_tmpptr[Y++] = palette_and_width; 
+        _ms_tmpptr[Y] = _tiling_tmp; 
         _ms_tmpptr2 += _tiling_width;
     }
+    for (; _ms_tmp < _MS_DLL_ARRAY_SIZE; _ms_tmp++) {
+        _ms_tmpptr = _ms_dls[X++]; 
+        Y = 3; 
+        _ms_tmpptr[Y++] = palette_and_width | 0x1f; 
+        _ms_tmpptr[Y] = 160; // Out of screen 
+    }
+
     _ms_vscroll_offset = _tiling_yoffset;
     _ms_vertical_scrolling_adjust_bottom_of_screen();
 }
+#undef palette_and_width
+#undef bottom
 
 #endif
 
