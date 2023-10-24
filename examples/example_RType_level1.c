5955012042d00221ddc2e8dbdd9e35463954e8b9
Bruno STEUX <bruno.steux@gmail.com>
2023-10-24 22:52:02 +0200

    making progress on RType

---
+++ examples/example_RType_level1.c
@@ -0,0 +1,46 @@
+const char tilemap_level1_0_0[25] = {78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 120, 122, 0, 2, 4, 6, 120, 122, 120, 122, 120, 122, 120, 122};
+const char tilemap_level1_0_data[] = {24, 0, tilemap_level1_0_0, 0x60, tilemap_level1_0_0 >> 8, (4 << 5) | ((-25) & 0x1f), 119, 96, 0xff};
+const char tilemap_level1_1_0[10] = {100, 102, 104, 106, 108, 110, 112, 114, 116, 118};
+const char tilemap_level1_1_1[4] = {24, 26, 28, 30};
+const char tilemap_level1_1_data[] = {9, 0, tilemap_level1_1_0, 0x60, tilemap_level1_1_0 >> 8, (4 << 5) | ((-10) & 0x1f), 51, 16, 13, tilemap_level1_1_1, 0x60, tilemap_level1_1_1 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 96, 0xff};
+const char tilemap_level1_2_0[4] = {120, 122, 120, 122};
+const char tilemap_level1_2_1[5] = {124, 126, 128, 130, 132};
+const char tilemap_level1_2_2[4] = {24, 48, 50, 52};
+const char tilemap_level1_2_3[1] = {236};
+const char tilemap_level1_2_data[] = {3, 0, tilemap_level1_2_0, 0x60, tilemap_level1_2_0 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 9, 5, tilemap_level1_2_1, 0x60, tilemap_level1_2_1 >> 8, (4 << 5) | ((-5) & 0x1f), 29, 16, 13, tilemap_level1_2_2, 0x60, tilemap_level1_2_2 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 17, 17, tilemap_level1_2_3, 0x60, tilemap_level1_2_3 >> 8, (7 << 5) | ((-1) & 0x1f), 11, 96, 0xff};
+const char tilemap_level1_3_0[2] = {204, 206};
+const char tilemap_level1_3_1[2] = {134, 136};
+const char tilemap_level1_3_2[4] = {24, 54, 56, 58};
+const char tilemap_level1_3_3[1] = {242};
+const char tilemap_level1_3_data[] = {6, 5, tilemap_level1_3_0, 0x60, tilemap_level1_3_0 >> 8, (6 << 5) | ((-2) & 0x1f), 15, 8, 7, tilemap_level1_3_1, 0x60, tilemap_level1_3_1 >> 8, (4 << 5) | ((-2) & 0x1f), 15, 16, 13, tilemap_level1_3_2, 0x60, tilemap_level1_3_2 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 17, 17, tilemap_level1_3_3, 0x60, tilemap_level1_3_3 >> 8, (7 << 5) | ((-1) & 0x1f), 11, 96, 0xff};
+const char tilemap_level1_4_0[4] = {24, 60, 62, 64};
+const char tilemap_level1_4_data[] = {16, 13, tilemap_level1_4_0, 0x60, tilemap_level1_4_0 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 96, 0xff};
+const char tilemap_level1_5_data[] = {96, 0xff};
+const char tilemap_level1_8_0[4] = {24, 2, 4, 6};
+const char tilemap_level1_8_data[] = {16, 13, tilemap_level1_8_0, 0x60, tilemap_level1_8_0 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 96, 0xff};
+const char tilemap_level1_9_0[2] = {214, 216};
+const char tilemap_level1_9_1[2] = {194, 196};
+const char tilemap_level1_9_data[] = {6, 5, tilemap_level1_9_0, 0x60, tilemap_level1_9_0 >> 8, (6 << 5) | ((-2) & 0x1f), 15, 8, 7, tilemap_level1_9_1, 0x60, tilemap_level1_9_1 >> 8, (4 << 5) | ((-2) & 0x1f), 15, 16, 13, tilemap_level1_1_1, 0x60, tilemap_level1_1_1 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 96, 0xff};
+const char tilemap_level1_10_0[4] = {180, 182, 180, 182};
+const char tilemap_level1_10_1[5] = {184, 186, 188, 190, 192};
+const char tilemap_level1_10_data[] = {3, 0, tilemap_level1_10_0, 0x60, tilemap_level1_10_0 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 9, 5, tilemap_level1_10_1, 0x60, tilemap_level1_10_1 >> 8, (4 << 5) | ((-5) & 0x1f), 29, 16, 13, tilemap_level1_2_2, 0x60, tilemap_level1_2_2 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 96, 0xff};
+const char tilemap_level1_11_0[10] = {160, 162, 164, 166, 168, 170, 172, 174, 176, 178};
+const char tilemap_level1_11_data[] = {9, 0, tilemap_level1_11_0, 0x60, tilemap_level1_11_0 >> 8, (4 << 5) | ((-10) & 0x1f), 51, 16, 13, tilemap_level1_3_2, 0x60, tilemap_level1_3_2 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 96, 0xff};
+const char tilemap_level1_12_0[11] = {138, 140, 142, 144, 146, 148, 150, 152, 154, 156, 158};
+const char tilemap_level1_12_1[2] = {220, 220};
+const char tilemap_level1_12_2[4] = {0, 60, 62, 64};
+const char tilemap_level1_12_3[8] = {222, 224, 230, 220, 220, 220, 226, 228};
+const char tilemap_level1_12_data[] = {10, 0, tilemap_level1_12_0, 0x60, tilemap_level1_12_0 >> 8, (4 << 5) | ((-11) & 0x1f), 56, 12, 11, tilemap_level1_12_1, 0x60, tilemap_level1_12_1 >> 8, (6 << 5) | ((-2) & 0x1f), 15, 16, 13, tilemap_level1_12_2, 0x60, tilemap_level1_12_2 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 24, 17, tilemap_level1_12_3, 0x60, tilemap_level1_12_3 >> 8, (6 << 5) | ((-8) & 0x1f), 42, 96, 0xff};
+
+const char tilemap_level1_data_ptrs_high[13] = {tilemap_level1_0_data >> 8, tilemap_level1_1_data >> 8, tilemap_level1_2_data >> 8, tilemap_level1_3_data >> 8, tilemap_level1_4_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_8_data >> 8, tilemap_level1_9_data >> 8, tilemap_level1_10_data >> 8, tilemap_level1_11_data >> 8, tilemap_level1_12_data >> 8};
+
+const char tilemap_level1_data_ptrs_low[13] = {tilemap_level1_0_data & 0xff, tilemap_level1_1_data & 0xff, tilemap_level1_2_data & 0xff, tilemap_level1_3_data & 0xff, tilemap_level1_4_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_8_data & 0xff, tilemap_level1_9_data & 0xff, tilemap_level1_10_data & 0xff, tilemap_level1_11_data & 0xff, tilemap_level1_12_data & 0xff};
+
+const char *tilemap_level1_data_ptrs[2] = {tilemap_level1_data_ptrs_high, tilemap_level1_data_ptrs_low};
+
+/*
+#define TILING_HEIGHT 13
+#define TILING_WIDTH 512
+#include "sparse_tiling.h"
+*/
+
--- examples/example_horizontal_scrolling.c
+++ examples/example_horizontal_scrolling.c
@@ -1,7 +1,35 @@
 #define HORIZONTAL_SCROLLING
 #include "sparse_tiling.h"
 
+// Generated from sprites7800 RType_tiles.yaml
+#include "example_RType_tiles.c"
+
+// Generated from tiles7800 --sparse RType_tiles.yaml --varname tilemap_level1 RType_level1.tmx 
+#include "example_RType_level1.c"
+
 void main()
 {
-    multisprite_display_tiles();
+    multisprite_init();
+    multisprite_set_charbase(brown_tiles1);
+    multisprite_sparse_tiling(tilemap_level1_data_ptrs, 0, 0, 13);
+    multisprite_save();
+    
+    // Beige palette
+    *P4C1 = multisprite_color(0x12); 
+    *P4C2 = multisprite_color(0x15);
+    *P4C3 = multisprite_color(0x18); 
+    
+    // Rose palette
+    *P6C1 = multisprite_color(0x38); // Dark Rose
+    *P6C2 = multisprite_color(0x3c); // Rose 
+    *P6C3 = multisprite_color(0x3f); // Light Rose 
+
+    // Grey palette 
+    *P7C1 = 0x04; // Dark gray
+    *P7C2 = 0x08; // Medium gray
+    *P7C3 = 0x0c; // Dark gray
+
+    do {
+        multisprite_flip();
+    } while (1);
 }
