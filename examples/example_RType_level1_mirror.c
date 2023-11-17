76dbb1ecf4af86c5d55ee366388cfceb9444d69a
Bruno STEUX <bruno.steux@gmail.com>
2023-11-17 22:35:52 +0100

    tiling mirroring OK

--- examples/example_RType_level1_mirror.c
+++ examples/example_RType_level1_mirror.c
@@ -23,17 +23,35 @@ bank1 const char tilemap_level1_4_1[4] = {28, 106, 108, 110};
 bank1 const char tilemap_level1_4_2[1] = {183};
 bank1 const char tilemap_level1_4_data[] = {5, 4, tilemap_level1_4_0, 0x60, tilemap_level1_4_0 >> 8, (4 << 5) | ((-2) & 0x1f), 15, 23, 20, tilemap_level1_4_1, 0x60, tilemap_level1_4_1 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 24, 24, tilemap_level1_4_2, 0x60, tilemap_level1_4_2 >> 8, (7 << 5) | ((-1) & 0x1f), 11, 96, 0xff};
 bank1 const char tilemap_level1_5_data[] = {96, 0xff};
-bank1 const char tilemap_level1_8_data[] = {24, 24, tilemap_level1_3_2, 0x60, tilemap_level1_3_2 >> 8, (7 << 5) | ((-1) & 0x1f), 11, 96, 0xff};
-bank1 const char tilemap_level1_9_data[] = {24, 24, tilemap_level1_4_2, 0x60, tilemap_level1_4_2 >> 8, (7 << 5) | ((-1) & 0x1f), 11, 96, 0xff};
-bank1 const char tilemap_level1_12_0[7] = {164, 164, 164, 164, 164, 164, 164};
-bank1 const char tilemap_level1_12_1[4] = {164, 164, 164, 164};
-bank1 const char tilemap_level1_12_2[16] = {170, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172};
-bank1 const char tilemap_level1_12_3[3] = {170, 174, 176};
-bank1 const char tilemap_level1_12_data[] = {9, 3, tilemap_level1_12_0, 0x60, tilemap_level1_12_0 >> 8, (5 << 5) | ((-7) & 0x1f), 38, 19, 16, tilemap_level1_12_1, 0x60, tilemap_level1_12_1 >> 8, (5 << 5) | ((-4) & 0x1f), 24, 60, 45, tilemap_level1_12_2, 0x60, tilemap_level1_12_2 >> 8, (6 << 5) | ((-16) & 0x1f), 78, 63, 61, tilemap_level1_12_3, 0x60, tilemap_level1_12_3 >> 8, (6 << 5) | ((-3) & 0x1f), 20, 96, 0xff};
+bank1 const char tilemap_level1_8_0[4] = {29, 107, 109, 111};
+bank1 const char tilemap_level1_8_data[] = {23, 20, tilemap_level1_8_0, 0x60, tilemap_level1_8_0 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 24, 24, tilemap_level1_3_2, 0x60, tilemap_level1_3_2 >> 8, (7 << 5) | ((-1) & 0x1f), 11, 96, 0xff};
+bank1 const char tilemap_level1_9_0[4] = {29, 79, 81, 83};
+bank1 const char tilemap_level1_9_1[3] = {145, 147, 149};
+bank1 const char tilemap_level1_9_2[2] = {161, 163};
+bank1 const char tilemap_level1_9_data[] = {23, 20, tilemap_level1_9_0, 0x60, tilemap_level1_9_0 >> 8, (4 << 5) | ((-4) & 0x1f), 24, 24, 24, tilemap_level1_4_2, 0x60, tilemap_level1_4_2 >> 8, (7 << 5) | ((-1) & 0x1f), 11, 41, 39, tilemap_level1_9_1, 0x60, tilemap_level1_9_1 >> 8, (6 << 5) | ((-3) & 0x1f), 20, 42, 41, tilemap_level1_9_2, 0x60, tilemap_level1_9_2 >> 8, (4 << 5) | ((-2) & 0x1f), 15, 96, 0xff};
+bank1 const char tilemap_level1_10_0[16] = {29, 57, 59, 61, 133, 135, 133, 135, 133, 135, 133, 135, 133, 135, 133, 135};
+bank1 const char tilemap_level1_10_1[2] = {133, 135};
+bank1 const char tilemap_level1_10_2[2] = {141, 143};
+bank1 const char tilemap_level1_10_3[5] = {151, 153, 155, 157, 159};
+bank1 const char tilemap_level1_10_data[] = {35, 20, tilemap_level1_10_0, 0x60, tilemap_level1_10_0 >> 8, (4 << 5) | ((-16) & 0x1f), 78, 37, 36, tilemap_level1_10_1, 0x60, tilemap_level1_10_1 >> 8, (4 << 5) | ((-2) & 0x1f), 15, 40, 39, tilemap_level1_10_2, 0x60, tilemap_level1_10_2 >> 8, (6 << 5) | ((-2) & 0x1f), 15, 43, 39, tilemap_level1_10_3, 0x60, tilemap_level1_10_3 >> 8, (4 << 5) | ((-5) & 0x1f), 29, 96, 0xff};
+bank1 const char tilemap_level1_11_0[3] = {51, 53, 55};
+bank1 const char tilemap_level1_11_1[6] = {51, 53, 55, 51, 53, 55};
+bank1 const char tilemap_level1_11_2[16] = {29, 31, 33, 35, 37, 39, 41, 43, 113, 115, 117, 113, 115, 117, 113, 115};
+bank1 const char tilemap_level1_11_3[8] = {117, 119, 121, 123, 125, 127, 129, 131};
+bank1 const char tilemap_level1_11_data[] = {2, 0, tilemap_level1_11_0, 0x60, tilemap_level1_11_0 >> 8, (4 << 5) | ((-3) & 0x1f), 20, 15, 10, tilemap_level1_11_1, 0x60, tilemap_level1_11_1 >> 8, (4 << 5) | ((-6) & 0x1f), 33, 35, 20, tilemap_level1_11_2, 0x60, tilemap_level1_11_2 >> 8, (4 << 5) | ((-16) & 0x1f), 78, 43, 36, tilemap_level1_11_3, 0x60, tilemap_level1_11_3 >> 8, (4 << 5) | ((-8) & 0x1f), 42, 96, 0xff};
+bank1 const char tilemap_level1_12_0[3] = {23, 25, 27};
+bank1 const char tilemap_level1_12_1[7] = {164, 164, 164, 164, 164, 164, 164};
+bank1 const char tilemap_level1_12_2[6] = {23, 25, 27, 23, 25, 27};
+bank1 const char tilemap_level1_12_3[4] = {164, 164, 164, 164};
+bank1 const char tilemap_level1_12_4[16] = {1, 3, 5, 7, 9, 11, 13, 15, 85, 87, 89, 85, 87, 89, 85, 87};
+bank1 const char tilemap_level1_12_5[9] = {89, 91, 93, 95, 97, 99, 101, 103, 105};
+bank1 const char tilemap_level1_12_6[16] = {170, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172, 172};
+bank1 const char tilemap_level1_12_7[3] = {170, 174, 176};
+bank1 const char tilemap_level1_12_data[] = {2, 0, tilemap_level1_12_0, 0x60, tilemap_level1_12_0 >> 8, (4 << 5) | ((-3) & 0x1f), 20, 9, 3, tilemap_level1_12_1, 0x60, tilemap_level1_12_1 >> 8, (5 << 5) | ((-7) & 0x1f), 38, 15, 10, tilemap_level1_12_2, 0x60, tilemap_level1_12_2 >> 8, (4 << 5) | ((-6) & 0x1f), 33, 19, 16, tilemap_level1_12_3, 0x60, tilemap_level1_12_3 >> 8, (5 << 5) | ((-4) & 0x1f), 24, 35, 20, tilemap_level1_12_4, 0x60, tilemap_level1_12_4 >> 8, (4 << 5) | ((-16) & 0x1f), 78, 44, 36, tilemap_level1_12_5, 0x60, tilemap_level1_12_5 >> 8, (4 << 5) | ((-9) & 0x1f), 47, 60, 45, tilemap_level1_12_6, 0x60, tilemap_level1_12_6 >> 8, (6 << 5) | ((-16) & 0x1f), 78, 63, 61, tilemap_level1_12_7, 0x60, tilemap_level1_12_7 >> 8, (6 << 5) | ((-3) & 0x1f), 20, 96, 0xff};
 
-bank1 const char tilemap_level1_data_ptrs_high[13] = {tilemap_level1_0_data >> 8, tilemap_level1_1_data >> 8, tilemap_level1_2_data >> 8, tilemap_level1_3_data >> 8, tilemap_level1_4_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_8_data >> 8, tilemap_level1_9_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_12_data >> 8};
+bank1 const char tilemap_level1_data_ptrs_high[13] = {tilemap_level1_0_data >> 8, tilemap_level1_1_data >> 8, tilemap_level1_2_data >> 8, tilemap_level1_3_data >> 8, tilemap_level1_4_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_5_data >> 8, tilemap_level1_8_data >> 8, tilemap_level1_9_data >> 8, tilemap_level1_10_data >> 8, tilemap_level1_11_data >> 8, tilemap_level1_12_data >> 8};
 
-bank1 const char tilemap_level1_data_ptrs_low[13] = {tilemap_level1_0_data & 0xff, tilemap_level1_1_data & 0xff, tilemap_level1_2_data & 0xff, tilemap_level1_3_data & 0xff, tilemap_level1_4_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_8_data & 0xff, tilemap_level1_9_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_12_data & 0xff};
+bank1 const char tilemap_level1_data_ptrs_low[13] = {tilemap_level1_0_data & 0xff, tilemap_level1_1_data & 0xff, tilemap_level1_2_data & 0xff, tilemap_level1_3_data & 0xff, tilemap_level1_4_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_5_data & 0xff, tilemap_level1_8_data & 0xff, tilemap_level1_9_data & 0xff, tilemap_level1_10_data & 0xff, tilemap_level1_11_data & 0xff, tilemap_level1_12_data & 0xff};
 
 bank1 const char *tilemap_level1_data_ptrs[2] = {tilemap_level1_data_ptrs_high, tilemap_level1_data_ptrs_low};
 
