3db9c348d988aadc0926e47d4824f8615faef6e2
Bruno STEUX <bruno.steux@gmail.com>
2024-03-03 11:26:32 +0100

    making progress on 8 lines

--- headers/multisprite_8lines.h
+++ headers/multisprite_8lines.h
@@ -98,11 +98,13 @@ ramchip char _ms_dldma_save[_MS_DLL_ARRAY_SIZE];
             if (Y >= _MS_DL_LIMIT) { \
                 _ms_dmaerror++; \
             } else { \
-                _ms_tmpptr[Y++] = (gfx); \
+                _ms_tmp2 = Y++; \
                 _ms_tmpptr[Y++] = -width & 0x1f | (palette << 5); \
                 _ms_tmpptr[Y++] = ((gfx) >> 8) | _ms_tmp; \
                 _ms_tmpptr[Y++] = (x); \
                 _ms_dlend[X] = Y; \
+                _ms_tmpptr[Y] = 0; \
+                _ms_tmpptr[Y = _ms_tmp2] = (gfx); \
                 if (_ms_tmp) { \
                     X++; \
                     _MS_DMA_CHECK((8 + width * 3 + 1) / 2) { \
@@ -111,11 +113,13 @@ ramchip char _ms_dldma_save[_MS_DLL_ARRAY_SIZE];
                         if (Y >= _MS_DL_LIMIT) { \
                             _ms_dmaerror++; \
                         } else { \
-                            _ms_tmpptr[Y++] = (gfx); \
+                            _ms_tmp2 = Y++; \
                             _ms_tmpptr[Y++] = -width & 0x1f | (palette << 5); \
                             _ms_tmpptr[Y++] = (((gfx) >> 8) - 0x08) | _ms_tmp; \
                             _ms_tmpptr[Y++] = (x); \
                             _ms_dlend[X] = Y; \
+                            _ms_tmpptr[Y] = 0; \
+                            _ms_tmpptr[Y = _ms_tmp2] = (gfx); \
                         } \
                     } \
                 }\
@@ -246,12 +250,14 @@ ramchip char _ms_dldma_save[_MS_DLL_ARRAY_SIZE];
             if (Y >= _MS_DL_LIMIT) { \
                 _ms_dmaerror++; \
             } else { \
-                _ms_tmpptr[Y++] = (gfx); \
+                _ms_tmp = Y++; \
                 _ms_tmpptr[Y++] = (mode)?0xc0:0x40; \
                 _ms_tmpptr[Y++] = ((gfx) >> 8); \
                 _ms_tmpptr[Y++] = -width & 0x1f | (palette << 5); \
                 _ms_tmpptr[Y++] = (x); \
                 _ms_dlend[X] = Y; \
+                _ms_tmpptr[Y] = 0; \
+                _ms_tmpptr[Y = _ms_tmp] = (gfx); \
             }\
         }
 
@@ -388,79 +394,87 @@ INIT_BANK void multisprite_init()
     multisprite_clear();
     multisprite_save();
 
-    _ms_tmpptr = _ms_b0_dll;
-    for (X = 0, _ms_tmp = 0; _ms_tmp <= 1; _ms_tmp++) {
-        // Build DLL
-        // 69 blank lines for PAL
-        // 19 blank lines for NTSC
-        if (_ms_pal_detected) {
-            // 16 blank lines
-            _ms_tmpptr[Y = 0] = 0x0f;  // 16 lines
-            _ms_tmpptr[++Y] = _ms_set_wm_dl >> 8;
-            _ms_tmpptr[++Y] = _ms_set_wm_dl;
-            // 16 blank lines
-            _ms_tmpptr[++Y] = 0x2f;  // 16 lines.. 8 high zone Holey DMA enabled just in case...
-            _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
-            _ms_tmpptr[++Y] = _ms_blank_dl;
-        } else {
-            _ms_tmpptr[Y = 0] = 0x28; // 9 lines. 8 high zone Holey DMA enabled just in case...
-            _ms_tmpptr[++Y] = _ms_set_wm_dl >> 8;
-            _ms_tmpptr[++Y] = _ms_set_wm_dl;
-        }
-        // 16 pixel high regions (14 regions = 224 pixels)
-        for (_ms_tmp2 = 0; _ms_tmp2 != 14; X++, _ms_tmp2++) {
-            _ms_tmpptr[++Y] = (_ms_tmp2 < _MS_TOP_DISPLAY)?0x0f:0x4f; // 16 lines
-            _ms_tmpptr[++Y] = _ms_dls[X] >> 8; // High address
-            _ms_tmpptr[++Y] = _ms_dls[X]; // Low address
-        }
-        // 1 pixel high region for the last display list (for vertical scrolling)
-        _ms_tmpptr[++Y] = 0x40; // 1 line
+    _ms_tmpptr = _ms_dll;
+    // Build DLL
+    if (_ms_pal_detected) {
+        // 16 blank lines
+        _ms_tmpptr[Y = 0] = 0x0f;  // 16 lines
+        _ms_tmpptr[++Y] = _ms_set_wm_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_set_wm_dl;
+        // 16 blank lines
+        _ms_tmpptr[++Y] = 0x0f;  // 16 lines
         _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
         _ms_tmpptr[++Y] = _ms_blank_dl;
-        X++; X++;
-        if (_ms_pal_detected) {
-            // 16 blank lines
-            _ms_tmpptr[++Y] = 0x2f;  // 16 lines. 8 high zone Holey DMA enabled just in case...
-            _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
-            _ms_tmpptr[++Y] = _ms_blank_dl;
-            // 16 blank lines
-            _ms_tmpptr[++Y] = 0x0f;  // 16 lines
-            _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
-            _ms_tmpptr[++Y] = _ms_blank_dl;
-            // 4 blank lines
-            _ms_tmpptr[++Y] = 0x03;  // 4 lines
-            _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
-            _ms_tmpptr[++Y] = _ms_blank_dl;
-        } else {
-            _ms_tmpptr[++Y] = 0x28; // 9 lines. 8 high zone Holey DMA enabled just in case...
-            _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
-            _ms_tmpptr[++Y] = _ms_blank_dl;
-        }
-        _ms_tmpptr = _ms_b1_dll;
+        // 14 blank lines
+        _ms_tmpptr[++Y] = 0x0d;  // 14 lines
+        _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_blank_dl;
+    } else {
+        _ms_tmpptr[Y = 0] = 0x0f; // 16 lines
+        _ms_tmpptr[++Y] = _ms_set_wm_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_set_wm_dl;
+        // 5 blank lines
+        _ms_tmpptr[++Y] = 0x04;  // 5 lines
+        _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_blank_dl;
     }
+    // 8 pixel high regions
+    for (X = 0; X != CONIO_NB_LINES; X++) {
+        _ms_tmpptr[++Y] = 0x27; // 8 lines, Holey DMA enabled
+        _ms_tmpptr[++Y] = _ms_dls[X] >> 8; // High address
+        _ms_tmpptr[++Y] = _ms_dls[X]; // Low address
+    }
+    if (_ms_pal_detected) {
+        // 16 blank lines
+        _ms_tmpptr[++Y] = 0x0f;  // 16 lines
+        _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_blank_dl;
+        // 16 blank lines
+        _ms_tmpptr[++Y] = 0x0f;  // 16 lines
+        _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_blank_dl;
+        // 15 blank lines
+        _ms_tmpptr[++Y] = 0x0e;  // 15 lines
+        _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_blank_dl;
+    } else {
+        _ms_tmpptr[++Y] = 0x0f; // 16 lines
+        _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_blank_dl;
+        // 6 blank lines
+        _ms_tmpptr[++Y] = 0x05;  // 6 lines
+        _ms_tmpptr[++Y] = _ms_blank_dl >> 8;
+        _ms_tmpptr[++Y] = _ms_blank_dl;
+    }
+    *DPPH = _ms_dll >> 8;
+    *DPPL = _ms_dll;
+    
     multisprite_start();
 }
 
 INIT_BANK void multisprite_clear()
 {
+    char *ptr;
+    Y = 0;
     for (X = _MS_DLL_ARRAY_SIZE - 1; X >= 0; X--) {
+        _ms_dlend[X] = 0;
         _ms_dlend_save[X] = 0;
 #ifdef DMA_CHECK
         _ms_dldma_save[X] = _MS_DMA_START_VALUE;
 #endif
+        ptr = ms_dls[X];
+        ptr[Y] = 0;
     }
 }
 
-// This one should be done during VBLANK, since we are copying from write buffer to currently displayed buffer
 INIT_BANK void multisprite_save()
 {
-    while (!(*MSTAT & 0x80)); // Wait for VBLANK
-        for (X = _MS_DLL_ARRAY_SIZE - 1; X >= 0; X--) {
-            _ms_dlend_save[X] = _ms_dlend[X];
+    for (X = _MS_DLL_ARRAY_SIZE - 1; X >= 0; X--) {
+        _ms_dlend_save[X] = _ms_dlend[X];
 #ifdef DMA_CHECK
-            _ms_dldma_save[X] = _ms_dldma[X];
+        _ms_dldma_save[X] = _ms_dldma[X];
 #endif
-        }
+    }
 }
 
 #ifdef MULTISPRITE_OVERLAY
@@ -473,20 +487,28 @@ void multisprite_save_overlay()
 
 void multisprite_clear_overlay()
 {
+    char *ptr;
     for (X = _MS_DLL_ARRAY_SIZE - 1; X >= 0; X--) {
         _ms_dlend_save_overlay[X] = _ms_dlend_save[X];
         _ms_dlend[X] = _ms_dlend_save[X];
+        Y = _ms_dlend[X];
+        ptr = ms_dls[X];
+        ptr[Y] = 0;
     }
 }
 #endif
 
 void multisprite_restore()
 {
+    char *ptr;
     for (X = _MS_DLL_ARRAY_SIZE - 1; X >= 0; X--) {
         _ms_dlend[X] = _ms_dlend_save[X];
 #ifdef DMA_CHECK
         _ms_dldma[X] = _ms_dldma_save[X];
 #endif
+        Y = _ms_dlend[X];
+        ptr = ms_dls[X];
+        ptr[Y] = 0;
     }
 }
 
